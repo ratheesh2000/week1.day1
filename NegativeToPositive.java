4e8f5f4c316a2453dc8e89978e4be697ef8842b2
ratheesh2000 <90104568+ratheesh2000@users.noreply.github.com>
2021-09-09 22:07:10 +0530

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,12 @@
+package week1.day1.assignments;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		int a=-39;
+		int b=-1*a;
+		System.out.println("The Number " +a+ " when converted to +ve is: "+b);
+
+	}
+
+}
